46983062fbc2f43ca58c0a2ad096848704718e3b
kota kawakami <kota160815@gmail.com>
2025-12-14 02:34:27 +0900

    fix:routerに伴いsuspenseを追加

--- game-record-app/app/boards/page.tsx
+++ game-record-app/app/boards/page.tsx
@@ -1,31 +1,15 @@
 "use client";
-import React from "react";
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import GameRecordView from "../components/GameRecordView";
 
-export const BoardsPage = () => {
-  const searchParams = useSearchParams();
-  const boardId = searchParams.get("board_id");
+import { Suspense } from "react";
+import BoardsPage from "../components/BoardsPage";
 
-  if (!boardId) {
-    return (
-      <div className="min-h-screen flex items-center justify-center">
-        <p>棋譜が選択されていません</p>
-      </div>
-    );
-  }
-
+export default function Page() {
   return (
-    <div className="min-h-screen">
-      <div className="p-8 pt-40 flex flex-col items-center justify-center">
-        <h1 className="text-xl font-bold">棋譜表示</h1>
-        <p>棋譜ID：{boardId}</p>
-        <GameRecordView boardId={boardId} />
-      </div>
-    </div>
-    
+    <Suspense fallback={<div>読み込み中...</div>}>
+      <BoardsPage />
+    </Suspense>
   );
 }
 
-export default BoardsPage;
+
+
